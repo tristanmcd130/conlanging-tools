b9f49f26a1bd1eac8cd81949616e30273368887b
tristanmcd130 <114033341+tristanmcd130@users.noreply.github.com>
2024-06-05 20:04:42 -0400

    Add files via upload

--- yawg.py
+++ yawg.py
@@ -1,53 +1,43 @@
-from random import choices, random
-from re import findall, sub
+import random, re
 
 class Choice:
-	def __init__(self, choices: list, weights: list[int | float] = []):
+	def __init__(self, choices, weights = None):
 		self.choices = choices
-		if weights:
-			assert len(weights) == len(choices)
+		if type(weights) == list:
+			assert len(weights) == len(choices), "weights must be the same length as choices"
 			self.weights = weights
 		else:
 			self.weights = [1/x for x in range(1, len(choices) + 1)]
-	def choose(self):
-		return choices(self.choices, weights = self.weights)[0]
+	def __call__(self):
+		return random.choices(self.choices, self.weights)[0]
 
 class Option:
-	def __init__(self, sequence: list, probability: float = 0.5):
-		self.sequence = sequence
-		self.probability = probability
-	def choose(self):
-		if random() < self.probability:
-			return self.sequence
+	def __init__(self, value, prob = 1/2):
+		self.value = value
+		assert 0 <= prob <= 1, "prob must be within [0, 1]"
+		self.prob = prob
+	def __call__(self):
+		if random.random() < self.prob:
+			return self.value
 		return ""
 
-def generate(shape: list, words: int = 1, rejections: list[str] = [], filters: list[tuple[str, str]] = []) -> list:
-	word_list = []
-	while len(word_list) < words:
-		queue = shape
-		while not all([type(x) == str for x in queue]):
-			new_queue = []
-			for item in queue:
-				if type(item) == str:
-					new_queue.append(item)
-				elif type(item) == list:
-					new_queue += item
-				elif type(item) in [Choice, Option]:
-					new_queue.append(item.choose())
-			queue = new_queue
-		word = "".join(queue)
-		if not any([findall(regex, word) for regex in rejections]) and word not in word_list:
-			for (regex, replacement) in filters:
-				word = sub(regex, replacement, word)
-			word_list.append(word)
-	return word_list
+def evaluate(word_shape):
+	if type(word_shape) in [Choice, Option]:
+		return evaluate(word_shape())
+	if type(word_shape) == list:
+		return "".join([evaluate(x) for x in word_shape])
+	if type(word_shape) == str:
+		return word_shape
+	raise ValueError(f"Invalid word shape {word_shape}")
 
-c = Choice("n t k m l r s ŋ d b p ᵐb w j ⁿd h ɡ f ᵑɡ".split(" "))
-v = Choice("a i u e o".split(" "))
-i = [Option(c, 2/3), v]
-s = [c, v]
-w = [Choice([[i, s], [i], [i, s, s]])]
-print("\n".join(generate(w, 100,
-	[r"(.+)\1", "ji", "wu"],
-	[("[ŋᵑ]", "ng"), ("ᵐ", "m"), ("j", "y"), ("ⁿ", "n"), ("ɡ", "g")]
-)))
+def generate(word_shape, num_words = 100, rejections = [], filters = {}):
+	words = []
+	while len(words) < num_words:
+		word = evaluate(word_shape)
+		if not any([re.search(r, word) for r in rejections]):
+			for (start, end) in filters.items():
+				word = re.sub(start, end, word)
+			if word not in words:
+				words.append(word)
+	random.shuffle(words)
+	return words
